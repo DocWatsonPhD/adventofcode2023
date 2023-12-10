af0bdc4c77366630a6935db0166293736ebda556
Stephen Watson <docwatsonphd@gmail.com>
2023-12-10 00:37:35 -0700

    Working part 5 day 2. Remove some extraneous code

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,22 +1,12 @@
 pub mod day5 {
-    use std::f64::INFINITY;
     use std::fs::File;
     use std::io::{BufReader, BufRead, Result};
     use std::u64::MAX;
     use std::vec;
 
-    #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
-    struct MapEntryD {
-        destination_start: u64,
-        source_start: u64,
-        destination_end: u64,
-        source_end: u64,
-        range_length: u64,
-    }
-
     // Re-order fields to sourt by source_start
     #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
-    struct MapEntryS {
+    struct MapEntry {
         source_start: u64,
         destination_start: u64,
         source_end: u64,
@@ -43,30 +33,13 @@ pub mod day5 {
         }
     }
 
-    // Could probably use a trait for the duplicate code but not really worth it for this
-    impl MapEntryD {
-        fn new(s: &str) -> MapEntryD {
+    impl MapEntry {
+        fn new(s: &str) -> MapEntry {
             let mut spl = s.split(' ');
             let dstart = spl.next().unwrap().trim().parse().unwrap();
             let sstart = spl.next().unwrap().trim().parse().unwrap();
             let rlength = spl.next().unwrap().trim().parse().unwrap();
-            MapEntryD {
-                destination_start: dstart,
-                source_start: sstart,
-                range_length: rlength,
-                destination_end: dstart + rlength - 1,
-                source_end: sstart + rlength - 1
-            }
-        }
-    }
-
-    impl MapEntryS {
-        fn new(s: &str) -> MapEntryS {
-            let mut spl = s.split(' ');
-            let dstart = spl.next().unwrap().trim().parse().unwrap();
-            let sstart = spl.next().unwrap().trim().parse().unwrap();
-            let rlength = spl.next().unwrap().trim().parse().unwrap();
-            MapEntryS {
+            MapEntry {
                 destination_start: dstart,
                 source_start: sstart,
                 range_length: rlength,
@@ -78,18 +51,12 @@ pub mod day5 {
 
     fn read_batch(
         reader: &mut BufReader<File>,
-        d_vec: &mut Vec<MapEntryD>,
-        s_vec: &mut Vec<MapEntryS>) {
+        s_vec: &mut Vec<MapEntry>) {
             let mut line: String = String::new();
             reader.read_line(&mut line)
                 .expect("Could not read line");
             while !line.trim().is_empty() {
-                let entry_d = MapEntryD::new(&line);
-                let entry_s: MapEntryS = MapEntryS::new(&line);
-                match d_vec.binary_search(&entry_d) {
-                    Ok(_) => {} // element already in vector @ `pos` 
-                    Err(pos) => d_vec.insert(pos, entry_d),
-                }
+                let entry_s: MapEntry = MapEntry::new(&line);
                 match s_vec.binary_search(&entry_s) {
                     Ok(_) => {} // element already in vector @ `pos` 
                     Err(pos) => s_vec.insert(pos, entry_s),
@@ -101,15 +68,15 @@ pub mod day5 {
         }
 
     fn fill_vecs(
-        seed_to_soil_d: &mut Vec<MapEntryD>, seed_to_soil_s: &mut Vec<MapEntryS>,
-        soil_to_fertilizer_d: &mut Vec<MapEntryD>, soil_to_fertilizer_s: &mut Vec<MapEntryS>,
-        fertilizer_to_water_d: &mut Vec<MapEntryD>, fertilizer_to_water_s: &mut Vec<MapEntryS>,
-        water_to_light_d: &mut Vec<MapEntryD>, water_to_light_s: &mut Vec<MapEntryS>,
-        light_to_temperature_d: &mut Vec<MapEntryD>, light_to_temperature_s: &mut Vec<MapEntryS>,
-        temperature_to_humidity_d: &mut Vec<MapEntryD>, temperature_to_humidity_s: &mut Vec<MapEntryS>,
-        humidity_to_location_d: &mut Vec<MapEntryD>, humidity_to_location_s: &mut Vec<MapEntryS> ) 
+        seed_to_soil: &mut Vec<MapEntry>,
+        soil_to_fertilizer: &mut Vec<MapEntry>,
+        fertilizer_to_water: &mut Vec<MapEntry>,
+        water_to_light: &mut Vec<MapEntry>,
+        light_to_temperature: &mut Vec<MapEntry>,
+        temperature_to_humidity: &mut Vec<MapEntry>,
+        humidity_to_location: &mut Vec<MapEntry> ) 
         -> Result<Vec<u64>> {
-        let file = File::open("C:\\Users\\docwa\\projects\\adventofcode2023\\src\\day5\\test_input.txt")
+        let file = File::open("C:\\Users\\docwa\\projects\\adventofcode2023\\src\\day5\\input.txt")
             .expect("What a terrible thing to have happened");
         let mut reader = BufReader::new(file);
 
@@ -124,59 +91,52 @@ pub mod day5 {
         reader.read_line(&mut line)?; // blank line
         reader.read_line(&mut line)?; // "seed-to-soil map:"
         line.clear();
-        read_batch(&mut reader, seed_to_soil_d, seed_to_soil_s);
+        read_batch(&mut reader, seed_to_soil);
 
         reader.read_line(&mut line)?; // "soil-to-fertilizer map: "
         line.clear();
-        read_batch(&mut reader, soil_to_fertilizer_d, soil_to_fertilizer_s);
+        read_batch(&mut reader, soil_to_fertilizer);
         
         reader.read_line(&mut line)?; // "fertilizer-to-water map: "
         line.clear();
-        read_batch(&mut reader, fertilizer_to_water_d, fertilizer_to_water_s);
+        read_batch(&mut reader, fertilizer_to_water);
 
         reader.read_line(&mut line)?; // "water-to-light map: "
         line.clear();
-        read_batch(&mut reader, water_to_light_d, water_to_light_s);
+        read_batch(&mut reader, water_to_light);
 
         reader.read_line(&mut line)?; // "light-to-temperature map: "
         line.clear();
-        read_batch(&mut reader, light_to_temperature_d, light_to_temperature_s);
+        read_batch(&mut reader, light_to_temperature);
 
         reader.read_line(&mut line)?; // "temperature-to-humidity map: "
         line.clear();
-        read_batch(&mut reader, temperature_to_humidity_d, temperature_to_humidity_s);
+        read_batch(&mut reader, temperature_to_humidity);
 
         reader.read_line(&mut line)?; // "humidity-to-location map: "
         line.clear();
-        read_batch(&mut reader, humidity_to_location_d, humidity_to_location_s);
+        read_batch(&mut reader, humidity_to_location);
 
         Ok(seeds)
     }
 
     pub fn part_one() -> Result<()> {
-        let mut seed_to_soil_d: Vec<MapEntryD> = vec![];
-        let mut seed_to_soil_s: Vec<MapEntryS> = vec![];
-        let mut soil_to_fertilizer_d: Vec<MapEntryD> = vec![];
-        let mut soil_to_fertilizer_s: Vec<MapEntryS> = vec![];
-        let mut fertilizer_to_water_d: Vec<MapEntryD> = vec![];
-        let mut fertilizer_to_water_s: Vec<MapEntryS> = vec![];
-        let mut water_to_light_d: Vec<MapEntryD> = vec![];
-        let mut water_to_light_s: Vec<MapEntryS> = vec![];
-        let mut light_to_temperature_d: Vec<MapEntryD> = vec![];
-        let mut light_to_temperature_s: Vec<MapEntryS> = vec![];
-        let mut temperature_to_humidity_d: Vec<MapEntryD> = vec![];
-        let mut temperature_to_humidity_s: Vec<MapEntryS> = vec![];
-        let mut humidity_to_location_d: Vec<MapEntryD> = vec![];
-        let mut humidity_to_location_s: Vec<MapEntryS> = vec![];
+        let mut seed_to_soil: Vec<MapEntry> = vec![];
+        let mut soil_to_fertilizer: Vec<MapEntry> = vec![];
+        let mut fertilizer_to_water: Vec<MapEntry> = vec![];
+        let mut water_to_light: Vec<MapEntry> = vec![];
+        let mut light_to_temperature: Vec<MapEntry> = vec![];
+        let mut temperature_to_humidity: Vec<MapEntry> = vec![];
+        let mut humidity_to_location: Vec<MapEntry> = vec![];
 
         let seeds = match fill_vecs(
-            &mut seed_to_soil_d, &mut seed_to_soil_s,
-            &mut soil_to_fertilizer_d, &mut soil_to_fertilizer_s,
-            &mut fertilizer_to_water_d, &mut fertilizer_to_water_s,
-            &mut water_to_light_d, &mut water_to_light_s,
-            &mut light_to_temperature_d, &mut light_to_temperature_s,
-            &mut temperature_to_humidity_d, &mut temperature_to_humidity_s,
-            &mut humidity_to_location_d, &mut humidity_to_location_s) {
+            &mut seed_to_soil,
+            &mut soil_to_fertilizer,
+            &mut fertilizer_to_water,
+            &mut water_to_light,
+            &mut light_to_temperature,
+            &mut temperature_to_humidity,
+            &mut humidity_to_location) {
                 Ok(val) => val,
                 Err(_) => vec![]
             };
@@ -184,21 +144,13 @@ pub mod day5 {
         // drill down from seed to location
         let mut min_location: u64 = MAX;
         for seed in seeds {
-            // println!("seed {}", seed);
-            let soil = get_mapped_dest_value(seed, &seed_to_soil_s);
-            // println!("\tsoil {}", soil);
-            let fertilizer = get_mapped_dest_value(soil, &soil_to_fertilizer_s);
-            // println!("\tfertilizer {}", fertilizer);
-            let water = get_mapped_dest_value(fertilizer, &fertilizer_to_water_s);
-            // println!("\twater {}", water);
-            let light = get_mapped_dest_value(water, &water_to_light_s);
-            // println!("\tlight {}", light);
-            let temperature = get_mapped_dest_value(light, &light_to_temperature_s);
-            // println!("\ttemperature {}", temperature);
-            let humidity = get_mapped_dest_value(temperature, &temperature_to_humidity_s);
-            // println!("\thumidity {}", humidity);
-            let location = get_mapped_dest_value(humidity, &humidity_to_location_s);
-            // println!("\tlocation {}", location);
+            let soil = get_mapped_dest_value(seed, &seed_to_soil);
+            let fertilizer = get_mapped_dest_value(soil, &soil_to_fertilizer);
+            let water = get_mapped_dest_value(fertilizer, &fertilizer_to_water);
+            let light = get_mapped_dest_value(water, &water_to_light);
+            let temperature = get_mapped_dest_value(light, &light_to_temperature);
+            let humidity = get_mapped_dest_value(temperature, &temperature_to_humidity);
+            let location = get_mapped_dest_value(humidity, &humidity_to_location);
             if location < min_location { min_location = location; }
         }
 
@@ -207,7 +159,7 @@ pub mod day5 {
         Ok(())
     }
 
-    fn get_mapped_dest_value(target: u64, vec_s: &Vec<MapEntryS>) -> u64 {
+    fn get_mapped_dest_value(target: u64, vec_s: &Vec<MapEntry>) -> u64 {
         let mut min_index: usize = 0;
         let mut max_index = vec_s.len() - 1;
         let mut i: usize = max_index / 2;
@@ -235,59 +187,24 @@ pub mod day5 {
         target
     }
 
-    fn get_mapped_source_value(target: u64, vec_d: &Vec<MapEntryD>) -> u64 {
-        let mut min_index: usize = 0;
-        let mut max_index = vec_d.len() - 1;
-        let mut i: usize = max_index / 2;
-        let mut d_low: u64;
-        let mut d_hi: u64;
-        loop {
-            d_low = vec_d[i].destination_start;
-            d_hi = vec_d[i].destination_end;
-            if target < d_low {
-                if i == 0 { break; }
-                max_index = i - 1;
-                i = (max_index - min_index) / 2;
-                continue;
-            }
-            if target > d_hi {
-                min_index = i;
-                i = (max_index + min_index).div_ceil(2);
-                if max_index == min_index || i == vec_d.len() { break; }
-                continue;
-            }
-
-            return vec_d[i].source_start + (target - d_low); 
-        }
-
-        target
-    }
-
     pub fn part_two() -> Result<()>{
-        let mut seed_to_soil_d: Vec<MapEntryD> = vec![];
-        let mut seed_to_soil_s: Vec<MapEntryS> = vec![];
-        let mut soil_to_fertilizer_d: Vec<MapEntryD> = vec![];
-        let mut soil_to_fertilizer_s: Vec<MapEntryS> = vec![];
-        let mut fertilizer_to_water_d: Vec<MapEntryD> = vec![];
-        let mut fertilizer_to_water_s: Vec<MapEntryS> = vec![];
-        let mut water_to_light_d: Vec<MapEntryD> = vec![];
-        let mut water_to_light_s: Vec<MapEntryS> = vec![];
-        let mut light_to_temperature_d: Vec<MapEntryD> = vec![];
-        let mut light_to_temperature_s: Vec<MapEntryS> = vec![];
-        let mut temperature_to_humidity_d: Vec<MapEntryD> = vec![];
-        let mut temperature_to_humidity_s: Vec<MapEntryS> = vec![];
-        let mut humidity_to_location_d: Vec<MapEntryD> = vec![];
-        let mut humidity_to_location_s: Vec<MapEntryS> = vec![];
+        let mut seed_to_soil: Vec<MapEntry> = vec![];
+        let mut soil_to_fertilizer: Vec<MapEntry> = vec![];
+        let mut fertilizer_to_water: Vec<MapEntry> = vec![];
+        let mut water_to_light: Vec<MapEntry> = vec![];
+        let mut light_to_temperature: Vec<MapEntry> = vec![];
+        let mut temperature_to_humidity: Vec<MapEntry> = vec![];
+        let mut humidity_to_location: Vec<MapEntry> = vec![];
 
         let seeds = 
         match fill_vecs(
-            &mut seed_to_soil_d, &mut seed_to_soil_s,
-            &mut soil_to_fertilizer_d, &mut soil_to_fertilizer_s,
-            &mut fertilizer_to_water_d, &mut fertilizer_to_water_s,
-            &mut water_to_light_d, &mut water_to_light_s,
-            &mut light_to_temperature_d, &mut light_to_temperature_s,
-            &mut temperature_to_humidity_d, &mut temperature_to_humidity_s,
-            &mut humidity_to_location_d, &mut humidity_to_location_s) {
+            &mut seed_to_soil,
+            &mut soil_to_fertilizer,
+            &mut fertilizer_to_water,
+            &mut water_to_light,
+            &mut light_to_temperature,
+            &mut temperature_to_humidity,
+            &mut humidity_to_location) {
                 Ok(val) => val,
                 Err(_) => vec![]
         };
@@ -301,62 +218,50 @@ pub mod day5 {
 
         // drill down from seed to location
         let mut min_location: u64 = MAX;
-        // Commented out because it doesn't work.
-        // Initially planned to brute-force every number within ranges inside seeds,
-        // but there are way too many to run through that the program takes a helluva
-        // long time to run. Probably gotta re-approach the problem.
-        // for seed in &seeds {
-        //     let sr = get_seed_range_for_soil(seed, &seed_to_soil_s);
-        //     for seed_value in sr {
-        //     // for seed_value in seed.source_start..seed.source_end {
-        //         // println!("seed {}", seed_value);
-        //         let soil = get_mapped_dest_value(seed_value, &seed_to_soil_s);
-        //         // println!("\tsoil {}", soil);
-        //         let fertilizer = get_mapped_dest_value(soil, &soil_to_fertilizer_s);
-        //         // println!("\tfertilizer {}", fertilizer);
-        //         let water = get_mapped_dest_value(fertilizer, &fertilizer_to_water_s);
-        //         // println!("\twater {}", water);
-        //         let light = get_mapped_dest_value(water, &water_to_light_s);
-        //         // println!("\tlight {}", light);
-        //         let temperature = get_mapped_dest_value(light, &light_to_temperature_s);
-        //         // println!("\ttemperature {}", temperature);
-        //         let humidity = get_mapped_dest_value(temperature, &temperature_to_humidity_s);
-        //         // println!("\thumidity {}", humidity);
-        //         let location = get_mapped_dest_value(humidity, &humidity_to_location_s);
-        //         // println!("\tlocation {}", location);
-        //         if location < min_location { min_location = location; }
-        //     }
-        // }
+        for seed in &seeds {
+            let seed_ranges = get_seed_range_for_soil(seed, &seed_to_soil);
+            for seed_range in seed_ranges {
+                for seed_value in seed_range.0..seed_range.1 {
+                    let soil = get_mapped_dest_value(seed_value, &seed_to_soil);
+                    let fertilizer = get_mapped_dest_value(soil, &soil_to_fertilizer);
+                    let water = get_mapped_dest_value(fertilizer, &fertilizer_to_water);
+                    let light = get_mapped_dest_value(water, &water_to_light);
+                    let temperature = get_mapped_dest_value(light, &light_to_temperature);
+                    let humidity = get_mapped_dest_value(temperature, &temperature_to_humidity);
+                    let location = get_mapped_dest_value(humidity, &humidity_to_location);
+                    if location < min_location { min_location = location; }
+                }
+            }
+        }
         
         println!("{}", min_location);
 
         Ok(())
     }
     
-    // Just returns every boundary where the seed entry matches to the given seed_to_soil map.
-    // Unfortunately this doesn't work because even in test data the solution lied within the bounds
-    fn get_seed_range_for_soil(seed: &SeedEntry, seed_to_soil_s: &Vec<MapEntryS>) -> Vec<u64> {
+    // Just returns every overlapping range where the seed entry matches to the given seed_to_soil map.
+    fn get_seed_range_for_soil(seed: &SeedEntry, seed_to_soil: &Vec<MapEntry>) -> Vec<(u64, u64)> {
         let seed_low: u64 = seed.source_start;
         let seed_hi: u64 = seed.source_end;
-        let mut result: Vec<u64> = vec![];
-        for soil in seed_to_soil_s {
+        let mut result: Vec<(u64, u64)> = vec![];
+        for soil in seed_to_soil {
             let seed_low_bounded: bool = seed_low >= soil.source_start && seed_low <= soil.source_end;
             let seed_hi_bounded: bool = seed_hi >= soil.source_start && seed_hi <= soil.source_end;
             if seed_low_bounded && seed_hi_bounded { 
-                result.push(soil.source_start.max(seed_low));
-                result.push(soil.source_end.min(seed_hi));
+                result.push((soil.source_start.max(seed_low),
+                             soil.source_end.min(seed_hi)));
             } else if seed_low_bounded && !seed_hi_bounded {
-                result.push(soil.source_start.max(seed_low));
-                result.push(soil.source_end);
+                result.push((soil.source_start.max(seed_low),
+                             soil.source_end));
             } else if !seed_low_bounded && seed_hi_bounded {
-                result.push(soil.source_start);
-                result.push(soil.source_end.min(seed_hi));
+                result.push((soil.source_start,
+                             soil.source_end.min(seed_hi)));
             } else if soil.source_start > seed_low && soil.source_end < seed_hi {
                 if seed_hi < soil.source_start || seed_low > soil.source_end {
                     continue;
                 }
-                result.push(soil.source_start);
-                result.push(soil.source_end);
+                result.push((soil.source_start,
+                             soil.source_end));
             }
         }
 
